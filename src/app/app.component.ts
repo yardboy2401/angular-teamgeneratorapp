72e6811a7fce9680d90f56abdf0809270f029d7c
Jeff Wymore <yardboy2401@gmail.com>
2022-06-28 14:28:44 -0700

    app functionality complete still need to break out to separate components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   newMemberName = '';
   members: string[] = [];
   errorMessage = '';
+  numberOfTeams: number | "" = "";
+  teams: string[][] = []
 
   onInput(member: string) {
     this.newMemberName = member;
@@ -24,5 +26,39 @@ export class AppComponent {
     this.members.push(this.newMemberName)
     this.newMemberName = '';
     this.errorMessage = '';
+  }
+
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value)
+  }
+
+  generateTeams() {
+    if(!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMessage = "Invalid Input"
+      return
+    }
+
+    if(this.members.length < this.numberOfTeams) {
+      this.errorMessage = "Not enough members";
+      return;
+    }
+    this.errorMessage = "";
+    const allMembers = [...this.members]
+
+    while(allMembers.length) {
+      for(let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length)
+        const member = allMembers.splice(randomIndex, 1)[0];
+  
+        if(!member) break;
+        if(this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+    this.members = [];
+    this.numberOfTeams = "";
   }
 }
